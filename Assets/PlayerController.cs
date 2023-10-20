0d35d5d6f5bd2877881e46b94d7ce04fa704a020
Sergey <Sergeev_sergey99@mail.ru>
2023-10-20 18:48:25 +0300

    Штуки

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,25 +9,38 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 0.2f;
 
     [SerializeField, ReadOnly] private Rigidbody2D rb; 
+    bool isCanMove = true;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (!isCanMove) return;
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             rb.MovePosition(transform.position + Vector3.up * speed);
+            StartCoroutine(WaitForMove());
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKey(KeyCode.DownArrow))
         {
             rb.MovePosition(transform.position + Vector3.down * speed);
+            StartCoroutine(WaitForMove());
         }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.MovePosition(transform.position + Vector3.left * speed);
+            StartCoroutine(WaitForMove());
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.MovePosition(transform.position + Vector3.right * speed);
+            StartCoroutine(WaitForMove());
         }
+    }
+
+    IEnumerator WaitForMove()
+    {
+        isCanMove = false;
+        yield return new WaitForSeconds(0.2f);
+        isCanMove = true;
     }
 
     private void OnValidate()
